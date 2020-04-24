ee07b4012ae882dbdad9b9d8a3537a4333f90949
arjenkins <arobinsonjenkins@gmail.com>
2020-04-23 22:48:11 -0700

    fix:fixed delete button

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -20,33 +20,49 @@ class ViewOrders extends Component {
             });
     }
 
-    render() {
-        return (
-            <Template>
-                <div className="container-fluid">
-                    {this.state.orders.map(order => {
-                        const createdDate = new Date(order.createdAt);
-                        return (
-                            <div className="row view-order-container" key={order._id}>
-                                <div className="col-md-4 view-order-left-col p-3">
-                                    <h2>{order.order_item}</h2>
-                                    <p>Ordered by: {order.ordered_by || ''}</p>
-                                </div>
-                                <div className="col-md-4 d-flex view-order-middle-col">
-                                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
-                                    <p>Quantity: {order.quantity}</p>
-                                 </div>
-                                 <div className="col-md-4 view-order-right-col">
-                                     <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
-                                 </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </Template>
-        );
-    }
-}
+    deleteOrder(order) {
+       // event.preventDefault();
+            fetch('${SERVER_IP}/api/delete-order',{
+                method: 'POST',
+                body: JSON.stringify({
+                id: order._id
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+                .then(response => console.log("Success", JSON.stringify(response)))
+                .catch(error => console.error(error));
+        
+        }
 
-export default ViewOrders;
+        render() {
+            return (
+                <Template>
+                    <div className="container-fluid">
+                        {this.state.orders.map(order => {
+                            const createdDate = new Date(order.createdAt);
+                            return (
+                                <div className="row view-order-container" key={order._id}>
+                                    <div className="col-md-4 view-order-left-col p-3">
+                                        <h2>{order.order_item}</h2>
+                                        <p>Ordered by: {order.ordered_by || ''}</p>
+                                    </div>
+                                    <div className="col-md-4 d-flex view-order-middle-col">
+                                        <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
+                                        <p>Quantity: {order.quantity}</p>
+                                     </div>
+                                     <div className="col-md-4 view-order-right-col">
+                                         <button className="btn btn-success">Edit</button>
+                                         <button className="btn btn-danger" onClick={()=> this.deleteOrder(order)}>Delete</button>
+                                     </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </Template>
+            );
+        }
+    }
+    export default ViewOrders;
